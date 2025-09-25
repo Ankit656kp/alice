4ccf7f7768d381fa31db68add559c403146b8c28
Ankit kumar <ankitgupta2ndsp@gmail.com>
2025-09-25 07:18:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -7,7 +7,7 @@ from pyrogram import filters
 load_dotenv()
 
 # Get this value from my.telegram.org/apps
-API_ID = API_ID
+API_ID = "API_ID"
 API_HASH = "API_HASH"
 
 # Get your token from @BotFather on Telegram.
@@ -112,4 +112,5 @@ if SUPPORT_GROUP:
         raise SystemExit(
             "[ERROR] - Your SUPPORT_GROUP url is wrong. Please ensure that it starts with https://"
         )
+
 
